1c683e990ada017d7ee1240596538af0e7e43174
pengzhanlee <cdlipengzhan@jd.com>
2017-09-07 15:53:26 +0800

    package

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+var path = require('path');
+var webpack = require('webpack');
+var production = process.env.NODE_ENV === 'production';
+
+module.exports = {
+    entry: {
+        'xeact': './src/index.js',
+    },
+
+    output: {
+        path: path.resolve(__dirname, 'dist' ),
+        filename: production ? '[name].min.js' : '[name].js',
+        library: 'xeact',
+        libraryTarget: 'umd'
+    },
+    devtool: production ? 'source-map' : '',
+    module: {
+        loaders: [
+            {
+                test: /\.js/,
+                loader: 'babel-loader?cacheDirectory',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            'xeact': path.resolve(__dirname, 'src/index'),
+        }
+    },
+    externals: [
+        {
+            react: {
+                root: 'React',
+                commonjs2: 'react',
+                commonjs: 'react',
+                amd: 'react',
+            },
+        },
+        {
+            'react-dom': {
+                root: 'ReactDOM',
+                commonjs2: 'react-dom',
+                commonjs: 'react-dom',
+                amd: 'react-dom',
+            },
+        },
+    ],
+    plugins: production ? [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: { warnings: false }
+        })
+    ] : []
+};
