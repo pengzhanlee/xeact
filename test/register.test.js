6e057925b696e1650fe4b9ca0b5e613d5df5aa1e
pengzhanlee <live@jsmix.com>
2018-01-11 17:21:24 +0800

    add raf

--- test/register.test.js
+++ test/register.test.js
@@ -1,7 +1,5 @@
 import React from "react";
-import xeact, {PureComponent} from "xeact";
-import Enzyme, {shallow} from "enzyme";
-import Adapter from 'enzyme-adapter-react-16';
+import {shallow} from "enzyme";
 import Sample from "./sample";
 
 console.debug = jest.genMockFunction();
@@ -10,11 +8,9 @@ console.info = jest.genMockFunction();
 console.warn = jest.genMockFunction();
 console.error = jest.genMockFunction();
 
-Enzyme.configure({ adapter: new Adapter() });
-
 test('render a basic component', () => {
     const wrapper = shallow(
         <Sample />
     );
-    expect(wrapper.length);
+    expect(wrapper.length).toBe(1);
 });
