d6cd3ff410cc92abce662eca41325ac2451653e1
pengzhanlee <pengzhan.lee@gmail.com>
2021-07-14 10:35:43 +0800

    update build script

---
+++ rollup.config.js
@@ -0,0 +1,48 @@
+import pkg from './package.json';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
+
+const extensions = ['.js'];
+const input = 'src/index.js';
+const external = Object.keys(pkg.peerDependencies);
+
+const plugins = [
+    nodeResolve({
+        extensions,
+        browser: true
+    }),
+    babel({
+        exclude: 'node_modules/**',
+        babelHelpers: 'bundled',
+        extensions,
+    }),
+    commonjs(),
+    replace({
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
+];
+
+export default [
+    {
+        input,
+        output: {
+            file: pkg.module,
+            format: 'esm',
+            sourcemap: true,
+        },
+        plugins,
+        external
+    },
+    {
+        input,
+        output: {
+            file: pkg.main,
+            format: 'cjs',
+            sourcemap: true,
+        },
+        plugins,
+        external
+    },
+];
